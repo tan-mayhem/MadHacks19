3bb589ee4479ce0359d572b233bd97a25edfd785
jackhwolf <jwolf22@wisc.edu>
2019-10-19 17:10:34 -0500

    working

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+import ddb
 
+
+# setup ddb
+udb = ddb.userddbconn()
+udb.create()
+sdb = ddb.saleddbconn()
+sdb.create()
+
+
+#######
+# API #
+#######
+
+# setup fastapi app
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -10,11 +25,62 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# define routes
+
 @app.get("/test")
 async def root():
-    return {"Message": "Hello!"}
+    ''' to test if server is working '''
+    return {"Message": "I'm working!"}
 
-@app.post("/user/{id}")
-async def user(id: str):
-    # check if user exists
-    return {'Response': -1}
+@app.post("/userexists")
+async def userexists(req: Request):
+    '''
+    <h3>check if user exists\n
+    required: `username` and `password`</h3>
+    '''
+    req = await req.body()
+    req = req.decode('utf-8')
+    req = {k.split('=')[0]:k.split('=')[1] for k in req.split('&')}
+    return udb.doesUserExist(**req)
+
+@app.post("/adduser")
+async def adduser(req: Request):
+    '''
+    <h3>add user\n
+    required: `username`, `password`, `phone`</h3>
+    '''
+    req = await req.body()
+    req = req.decode('utf-8')
+    req = {k.split('=')[0]:k.split('=')[1] for k in req.split('&')}
+    return udb.signUpUser(**req)
+
+@app.post("/addforsale")
+async def addlandforsale(req: Request):
+    '''
+    <h3>add land for sale\n
+    required params:
+        - `seller`:     username of seller
+        - `title`:      title of post
+        - `location`:   street address of plot
+        - `size`:       size of land in acres
+        - `price`:      price of land per acre
+    optional params:
+        - `shortdesc`:  short description of land/usage/etc
+        - `longdesc`:   longer description</h3>
+    '''
+    req = await req.body()
+    req = req.decode('utf-8')
+    req = {k:v for k,v in map(lambda x: x.split('='), req.split('&'))}
+    return sdb.addSaleItem(**req)
+
+@app.get("/forsale")
+async def forsale(req: Request):
+    '''
+    <h3>See for sale postings\n
+    optional params:
+        - `keywords`: delim'd string of keywords. searches union
+        - `price`:    max price per acre to search
+        - to search by location:
+            - `loc`:  street address
+            - `rad`:  radius in miles to search within</h3>
+    '''
--- backend/ddb.py
+++ backend/ddb.py
@@ -1,2 +1,294 @@
+from time import time
 import boto3
+from boto3.dynamodb.conditions import Key, Attr, Not
 from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+#################
+# base db class #
+#################
+
+class db:
+    ''' subclass to handle db conn - throws errors '''
+
+    def __init__(self):
+        ''' get a dynamodb resource '''
+        self.res = boto3.resource(
+                        'dynamodb',
+                        aws_access_key_id=os.environ.get('access'),
+                        aws_secret_access_key=os.environ.get('secret'),
+                        region_name='us-west-1'
+                    )
+
+    def create_(self, tid, key_schema, attribute_defn, **kw):
+        ''' create table '''
+        self.res.create_table(
+            TableName=tid,
+            KeySchema=key_schema,
+            AttributeDefinitions=attribute_defn,
+            ProvisionedThroughput={
+                "ReadCapacityUnits": kw.get('RCU', 2),
+                "WriteCapacityUnits": kw.get('WCU', 2)
+            }
+        )
+
+    def put_(self, tid, item):
+        ''' put item(s) into table '''
+        table = self.res.Table(tid)
+        if not issubclass(type(item), list):
+            item = [item]
+        for i in item:
+            table.put_item(Item=i)
+
+    def query_(self, tid, keycondexpr):
+        ''' lookup something in table '''
+        table = self.res.Table(tid)
+        resp = table.query(KeyConditionExpression=keycondexpr)
+        return resp
+
+    def delete_(self, tid, keytodelete):
+        ''' delete something from table '''
+        table = self.res.Table(tid)
+        resp = table.delete_item(Key=keytodelete)
+        return resp
+
+    def scan_(self, tid, **kw):
+        resp = self.res.scan(TableName=)
+
+
+######################
+# class for ddb conn #
+######################
+
+class ddbconn(db):
+    ''' class to handle connection to user db '''
+
+    def __init__(self, tid, key_schema, attr_defn):
+        db.__init__(self)
+        self.tid = tid
+        self.key_schema = key_schema
+        self.attr_defn = attr_defn
+
+    def create(self, **kw):
+        ''' create table '''
+        try:
+            self.create_(
+                self.tid,
+                self.key_schema,
+                self.attr_defn, **kw
+            )
+            return True
+        except Exception as e:
+            return {'error': str(e)}
+
+    def put(self, item, **kw):
+        ''' put item into table and handle errors '''
+        try:
+            self.put_(
+                self.tid,
+                item
+            )
+            return True
+        except Exception as e:
+            return {'error': str(e)}
+
+    def query(self, keycondexpr, **kw):
+        ''' look up item in table and handle errors '''
+        try:
+            resp = self.query_(self.tid, keycondexpr)
+            return resp
+        except Exception as e:
+            return {'error': str(e)}
+
+    def delete(self, keytodelete, **kw):
+        ''' delete item from table and handle errors '''
+        try:
+            resp = self.delete_(self.tid, keytodelete)
+            return resp
+        except Exception as e:
+            return {'error': str(e)}
+
+
+######################
+# how we track users #
+######################
+
+class userddbconn(ddbconn):
+    ''' connect to user db '''
+
+    def __init__(self):
+        ''' define table name and schema, init connection '''
+        self.tid = 'users-madhacks2019'    # name of table
+        self.key_schema = [             # schema for keys
+                    {
+                        'AttributeName': 'username',
+                        'KeyType': 'HASH'
+                    },
+                    {
+                        'AttributeName': 'password',
+                        'KeyType': 'RANGE'
+                    }
+                ]
+        self.attr_defn = [              # what types are our keys
+                    {
+                        'AttributeName': 'username',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'password',
+                        'AttributeType': 'S'
+                    }
+                ]
+        ddbconn.__init__(self, self.tid, self.key_schema, self.attr_defn)
+
+    def hash__(self, x):
+        ''' return hashed version of x '''
+        return x
+
+    def fmtentry__(self, **kw):
+        '''
+        clean up our input into a standard format
+        args: username, password, email
+        '''
+        return {
+            'username':    self.hash__(kw.get('username')),
+            'password':    self.hash__(kw.get('password')),
+            'phone':       self.hash__(kw.get('phone')),
+            'forsale':     [],
+            'signupdate':  str(int(time()))
+        }
+
+    def getUser(self, **kw):
+        ''' get a user '''
+        return self.query(Key('username').eq(kw.get('username')))
+
+    def doesUserExist(self, **kw):
+        '''
+        is user in ddb?
+        args: username, password
+        '''
+        u = self.getUser(**kw)
+        if u.get('Count', -1) > 0:
+            if u.get('Items')[0]['password'] == kw['password']:
+                return {'Response': 1, 'meta': u.get('Items')[0]}
+        return {'Response': 0}
+
+    def signUpUser(self, **kw):
+        '''
+        add user to ddb
+        args: username, password, email
+        '''
+        if self.doesUserExist(**kw)['Response'] == 0:
+            p = self.put(self.fmtentry__(**kw))
+            if p:
+                return {'message': 'user signed up.',
+                        'meta': {'newuname': kw.get('username')}}
+            return {'message': 'failed to sign up user.',
+                        'meta': p}
+        return {'message': 'user already exists'}
+
+    def logInUser(self, **kw):
+        ''' if user exists, send them a cookie '''
+        if self.doesUserExist(**kw)['Response'] == 1:
+            return {'Result': 1, 'Cookie': 'COOKIE!!!'}
+        else:
+            return {'Result': 0, 'meta': {'message': 'User does not exist'}}
+
+    def deleteUser(self, **kw):
+        '''
+        delete user from ddb
+        args: username, password
+        '''
+        self.delete({
+                    'username': kw.get('username'),
+                    'password': kw.get('password')
+                })
+        return {'message': 'user deleted.'}
+
+    def getUserCreds(self, **kw):
+        ''' get username and password of user '''
+        u = self.getUser(**kw)
+        if u.get('Count', -1) > 0:
+            u = u.get('Items', [{}])[0]
+        return u.get('username', ''), u.get('password', '')
+
+
+    def addSaleItemToUser(self, **kw):
+        ''' add a sale item ID into user's `forsale` list '''
+        uname, pword = self.getUserCreds(**kw)
+        try:
+            table = self.res.Table(self.tid)
+            result = table.update_item(
+                Key={
+                    'username': uname,
+                    'password': pword
+                },
+                UpdateExpression="SET forsale = list_append(forsale, :i)",
+                ConditionExpression="NOT contains(forsale, :j)",
+                ExpressionAttributeValues={
+                    ':i': [kw.get('id_')],
+                    ':j': kw.get('id_')
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            return {'Response': 1}
+        except Exception as e:
+            return {'Response': 0, 'Meta': {'Error': str(e)}}
+
+##############################
+# how we track land for sale #
+##############################
+
+class saleddbconn(ddbconn):
+    ''' connect to sale db '''
+
+    def __init__(self):
+        ''' define table name and schema, init connection '''
+        self.tid = 'sale-madhacks2019'    # name of table
+        self.key_schema = [             # schema for keys
+                    {
+                        'AttributeName': 'id',
+                        'KeyType': 'HASH'
+                    },
+                    {
+                        'AttributeName': 'seller',
+                        'KeyType': 'RANGE'
+                    }
+                ]
+        self.attr_defn = [              # what types are our keys
+                    {
+                        'AttributeName': 'id',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'seller',
+                        'AttributeType': 'S'
+                    }
+                ]
+        ddbconn.__init__(self, self.tid, self.key_schema, self.attr_defn)
+
+    def fmtentry__(self, **kw):
+        '''
+        clean up our input into a standard format
+        '''
+        return {
+            'id':        str(int(time())),
+            'seller':    kw.get('seller'),
+            'title':     kw.get('title'),
+            'location':  kw.get('location'),
+            'size':      kw.get('size'),
+            'price':     kw.get('price'),
+            'shortdesc': kw.get('shortdesc', ''),
+            'longdesc':  kw.get('longdesc', '')
+        }
+
+    def addSaleItem(self, **kw):
+        e = self.fmtentry__(**kw)
+        p = self.put(e)
+        if p:
+            userddbconn().addSaleItemToUser(username=e['seller'], id_=e['id'])
+            return {'Response': 1}
+        return {'Response': 0}
